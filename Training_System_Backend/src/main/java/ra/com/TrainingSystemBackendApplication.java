593bc98acd6a394944bd57e5e197be79350554fd
Đức Vũ <ducvu2509@gmail.com>
2024-03-09 15:54:15 +0700

    vendor: implement  project structure

---
+++ Training_System_Backend/src/main/java/ra/com/TrainingSystemBackendApplication.java
@@ -0,0 +1,13 @@
+package ra.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingSystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingSystemBackendApplication.class, args);
+    }
+
+}
